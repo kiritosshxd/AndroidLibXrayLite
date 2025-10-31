29da2b244ae3fba69e17d1b4e24a51551dbf7e9d
kiritosshxd <96794616+kiritosshxd@users.noreply.github.com>
2025-10-31 20:10:27 -0300

    Update libv2ray_protect.go

--- libv2ray_protect.go
+++ libv2ray_protect.go
@@ -6,15 +6,19 @@ import (
     v2internet "github.com/xtls/xray-core/transport/internet"
 )
 
+// Essa função será chamada no NewCoreController
 func attachProtectedDialer(cb CoreCallbackHandler) {
     if cb == nil {
         return
     }
 
-    d := NewProtectedDialer(cb) // vem do libv2ray_support.go que você copiou
+    // cb tem Protect(int) bool (a gente vai recolocar na interface)
+    d := NewProtectedDialer(cb)
+    // essa função existia no código da 25.3.31
     d.PrepareResolveChan()
 
+    // registra o dialer protegido no xray
     v2internet.UseAlternativeSystemDialer(d)
 
-    log.Println("[xray-lite] protected dialer enabled")
+    log.Println("[xray-lite] protected dialer enabled (vpn protect)")
 }
